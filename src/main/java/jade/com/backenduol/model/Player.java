b666cb6a528479048c72bc237cdf4137b491767d
jadeilton <jadeiltondm@gmail.com>
2024-01-09 15:12:42 -0300

    feat: create atributos in class Player

--- src/main/java/jade/com/backenduol/model/Player.java
+++ src/main/java/jade/com/backenduol/model/Player.java
@@ -1,4 +1,24 @@
 package jade.com.backenduol.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="players")
+@Table(name = "players")
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
 public class Player {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String codiname;
+    private TimeTyper timeTyper;
+
 }
