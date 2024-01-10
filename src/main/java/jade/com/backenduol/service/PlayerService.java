2135721ceffff79e629200e4b9e2105342e0f750
jadeilton <jadeiltondm@gmail.com>
2024-01-10 14:17:47 -0300

    feat: update PlayerService

--- src/main/java/jade/com/backenduol/service/PlayerService.java
+++ src/main/java/jade/com/backenduol/service/PlayerService.java
@@ -1,11 +1,15 @@
 package jade.com.backenduol.service;
 
 
+import jade.com.backenduol.infra.CodinameHandler;
 import jade.com.backenduol.model.Player;
+import jade.com.backenduol.model.TimeTyper;
 import jade.com.backenduol.model.dtos.PlayerDto;
 import jade.com.backenduol.repositorys.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlayerService {
@@ -14,9 +18,25 @@ public class PlayerService {
 
     @Autowired
     private PlayerRepository repository;
+
+    @Autowired
+    private CodinameHandler handler;
+
     public Player createPlayer(PlayerDto dto) {
 
         Player newPlayer = new Player(dto);
+        String codiname = getCodiname(dto.timeTyper());
+        newPlayer.setCodiname(codiname);
         return repository.save(newPlayer);
      }
+
+    private String getCodiname(TimeTyper timeTyper) {
+        return handler.findCodiname(timeTyper);
+    }
+
+    public List<Player> getAllPlayers(){
+
+        return repository.findAll();
+    }
+
 }
