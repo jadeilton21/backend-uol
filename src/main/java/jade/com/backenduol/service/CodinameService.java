4b3d5f6116be621fff84bd5bc1a74f24c3e8dcc1
jadeilton <jadeiltondm@gmail.com>
2024-01-10 14:17:04 -0300

    feat: update CondinameService

--- src/main/java/jade/com/backenduol/service/CodinameService.java
+++ src/main/java/jade/com/backenduol/service/CodinameService.java
@@ -10,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,31 +29,55 @@ public class CodinameService {
     private Environment env;
 
     private List<String> avangersCodinameList = new ArrayList<>();
-
+    private List<String> ligadajusticalis = new ArrayList<>();
     ObjectMapper objectMapper = new ObjectMapper();
 
 
     @PostConstruct
-    public void loadJsonDate(){
+    public void loadJsonDate() {
 
-        try{
+        try {
 
-            String codinameResponse = restTemplate.getForObject(env.getProperty("avangers"),String.class);
+            String codinameResponse = restTemplate.getForObject(env.getProperty("avangers"), String.class);
 
             JsonNode jsonNode = objectMapper.readTree(codinameResponse);
 
 
             ArrayNode avangers = (ArrayNode) jsonNode.get("vigadores");
-            for(JsonNode item: avangers){
+            for (JsonNode item : avangers) {
                 this.avangersCodinameList.add(item.get("codiname").asText());
 
             }
-        }catch (Exception e){
-            e.printStackTrace();;
+        } catch (Exception e) {
+            e.printStackTrace();
+            ;
+
+        }
+
+
+    }
+        @PostConstruct
+        public void loadXmlData(){
+
+            try{
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document document = builder.parse(env.getProperty("justice.league"));
+
+                NodeList codinameList = document.getElementsByTagName("codiname");
+
+                for(int i = 0; i< codinameList.getLength();i++){
+                    Element codinameElement = (Element) codinameList.item(i);
+                    String codiname = codinameElement.getTextContent();
+                    this.ligadajusticalis.add(codiname);
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
 
         }
 
 
 
     }
-}
