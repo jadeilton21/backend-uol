c73e8c5f08b0c69831410416e438cb10c063b84c
jadeilton <jadeiltondm@gmail.com>
2024-01-10 14:16:46 -0300

    feat: update CondinameHandler

--- src/main/java/jade/com/backenduol/infra/CodinameHandler.java
+++ src/main/java/jade/com/backenduol/infra/CodinameHandler.java
@@ -1,12 +1,26 @@
 package jade.com.backenduol.infra;
 
 
+import jade.com.backenduol.model.TimeTyper;
+import jade.com.backenduol.service.CodinameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CodinameHandler {
 
+    @Autowired
+    private CodinameService service;
 
 
-    public String
-}
+    public String findCodiname(TimeTyper timeTyper) {
+        if(timeTyper==TimeTyper.OS_VINGADORES){
+            String firstMatch = service.getAvangersCodinameList().stream().findFirst().orElseThrow();
+            this.service.getAvangersCodinameList().remove(firstMatch);
+            return firstMatch;
+        }
+        String firstMatch = service.getLigadajusticalis().stream().findFirst().orElseThrow();
+        this.service.getLigadajusticalis().remove(firstMatch);
+        return firstMatch;
+    }
+    }
