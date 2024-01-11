064812b6723f08d961c58055276936c7037998de
jadeilton <jadeiltondm@gmail.com>
2024-01-11 13:13:48 -0300

    feat: create test code home_cenario1.

--- src/test/java/jade/com/backenduol/controller/HomeControllerTest.java
+++ src/test/java/jade/com/backenduol/controller/HomeControllerTest.java
@@ -1,5 +1,7 @@
 package jade.com.backenduol.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class HomeControllerTest {
 
 
-
-
-
-
+    @Test
+    @DisplayName("Deve Devolver Código Http 404")
+    void home_cenario1() throws Exception{
+    }
 }
