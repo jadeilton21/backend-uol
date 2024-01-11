dba3d714d54d743dbf71ac29763b79e56869859a
jadeilton <jadeiltondm@gmail.com>
2024-01-11 14:37:31 -0300

    feat: create arguments in test method createPlayer_Cenario1.

--- src/test/java/jade/com/backenduol/controller/PlayerControllerTest.java
+++ src/test/java/jade/com/backenduol/controller/PlayerControllerTest.java
@@ -1,9 +1,16 @@
 package jade.com.backenduol.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -11,8 +18,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerControllerTest {
 
 
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    @DisplayName("Deve devolver Código 404 quando informações forem invalidas")
+    void createPlayer_cenario1() throws Exception{
+
+        var response = mockMvc.perform(
+                post("/players")
+        ).andReturn().getResponse();
 
 
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
 
-
+    }
 }
