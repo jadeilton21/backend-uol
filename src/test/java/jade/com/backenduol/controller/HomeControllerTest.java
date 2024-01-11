e75499c7158b8e858336272e81f9133f60b54f65
jadeilton <jadeiltondm@gmail.com>
2024-01-11 13:18:08 -0300

    feat: create arguments in method home_cenario1.

--- src/test/java/jade/com/backenduol/controller/HomeControllerTest.java
+++ src/test/java/jade/com/backenduol/controller/HomeControllerTest.java
@@ -2,11 +2,17 @@ package jade.com.backenduol.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -14,8 +20,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class HomeControllerTest {
 
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
     @Test
     @DisplayName("Deve Devolver Código Http 404")
     void home_cenario1() throws Exception{
+
+        var response =mockMvc.perform(
+                post("/")
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
