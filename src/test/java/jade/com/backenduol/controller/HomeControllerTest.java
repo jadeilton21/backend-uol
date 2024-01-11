63e1f0c23b66381065b2c2e22f5b9e7b0405b2b7
jadeilton <jadeiltondm@gmail.com>
2024-01-11 13:07:16 -0300

    feat: create class test HomeControllerTest

---
+++ src/test/java/jade/com/backenduol/controller/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package jade.com.backenduol.controller;
+
+import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+
+
+
+
+
+}
