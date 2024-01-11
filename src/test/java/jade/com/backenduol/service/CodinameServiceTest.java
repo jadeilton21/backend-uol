24bb0a9031e8b7d52f4885a08914fb09746ba7e2
jadeilton <jadeiltondm@gmail.com>
2024-01-11 14:57:33 -0300

    feat: create method test loadJsonDate_cenario1, tested.

--- src/test/java/jade/com/backenduol/service/CodinameServiceTest.java
+++ src/test/java/jade/com/backenduol/service/CodinameServiceTest.java
@@ -1,12 +1,18 @@
 package jade.com.backenduol.service;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.internal.verification.MockAwareVerificationMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,4 +22,17 @@ class CodinameServiceTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    @DisplayName("Deve Devolver Código 404 quando Informações estiverem invalidas....")
+    void loadJsonDate_cenario1() throws Exception {
+
+        var response = mockMvc
+                .perform(
+                        post("/players")
+                ).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
