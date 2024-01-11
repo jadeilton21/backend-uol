79fc871c2ba460290906d67f901a653a2506e3b4
jadeilton <jadeiltondm@gmail.com>
2024-01-11 14:33:32 -0300

    feat: create test Class PlayerControllerTest

---
+++ src/test/java/jade/com/backenduol/controller/PlayerControllerTest.java
@@ -0,0 +1,18 @@
+package jade.com.backenduol.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PlayerControllerTest {
+
+
+
+
+
+
+}
